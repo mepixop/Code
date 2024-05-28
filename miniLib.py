196e32a3254438da88beead69845f233dde53a9c
mepixop <mepixop@gmail.com>
2024-05-28 14:14:56 +0200

    ad new function  countfib

--- miniLib.py
+++ miniLib.py
@@ -1,18 +1,32 @@
-#count excercise numbers and print Title with the Number
+#count excercise numbers and print Title with the Number and optionaly description
 aufgaben_Nummer=0
-def printTitel(name=''):
+def printTitel(type=0,name=''):
   global aufgaben_Nummer
   aufgaben_Nummer +=1
-  print( "\n", "Aufgabe {}".format(aufgaben_Nummer), name)
+  types=(['','Übung','Aufgabe'])
 
-def printPrettyMultipliTable(nM):
+  print( "\n", "{}".format(aufgaben_Nummer), name)
+
+#prints Multiplikationtable nM-times
+def printPrettyMultipliTable(number):
   print("   ", end="")
-  for i in range(1, nM + 1):
+  for i in range(1, number + 1):
       print(f"{i:4}", end="")
-  print("\n" + "----" * (nM + 1))
+  print("\n" + "----" * (number + 1))
 
-  for i in range(1, nM + 1):
+  for i in range(1, number + 1):
       print(f"{i:2} |", end="")
-      for j in range(1, nM + 1):
+      for j in range(1, number + 1):
           print(f"{i * j:4}", end="")
       print()
+
+#Summe von Aufeinanderfolgenden positiven, Natürlichen Zahlen (Fibonacci)
+def countFib(number):
+  summe =0
+  if number < 0: 
+    print("Error: bitte nur Positive zahlen")
+  else:
+    for i in range (1, number+1):
+      summe += i
+
+    print("Summe:", str(summe))
